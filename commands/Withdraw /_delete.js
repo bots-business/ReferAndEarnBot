8c4099c386d8053a3626e91120d8fb7526944387
Bots.Business <git.sync@bots.business>
2025-04-20 19:46:01 +0700

    shorter code

--- commands/Withdraw /_delete.js
+++ commands/Withdraw /_delete.js
@@ -18,14 +18,14 @@ CMD*/
 
 // check if message is from a callback query and delete the message
 var messageId = request.message.message_id;
-if (messageId) {
-  Api.deleteMessage({
-    message_id: messageId,
-  });
+if (!messageId) return;
 
-  Api.answerCallbackQuery({
-    callback_query_id: request.id,
-    text: "✅ Message deleted.",
-    show_alert: false,
-  });
-}
+Api.deleteMessage({
+  message_id: messageId,
+});
+
+Api.answerCallbackQuery({
+  callback_query_id: request.id,
+  text: "✅ Message deleted.",
+  show_alert: false,
+});
