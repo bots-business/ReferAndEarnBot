e94ff1a9a792ecbafc93dedca7014b965b45bef9
Bots.Business <git.sync@bots.business>
2025-04-20 16:56:15 +0700

    code refactoring with sendMessage

--- commands/broadcast/_broadcast_status.js
+++ commands/broadcast/_broadcast_status.js
@@ -19,18 +19,21 @@ CMD*/
 // check if the user is an admin
 if(!checkAdminAccess()) return;
 
+let prms = {
+  reply_to_message_id: request.message_id,
+  parse_mode: "HTML"
+}
+
 var task_id = Bot.getProperty("broadcast_task_id");
+text = "No broadcast task found. Please create a new broadcast first.\n\nUse the command /broadcast to create a new broadcast task."
+
 if (!task_id) {
-  Api.sendMessage({
-    text: "No broadcast task found. Please create a new broadcast first.\n\nUse the command /broadcast to create a new broadcast task.",
-    reply_to_message_id: request.message_id,
-  });
+  prms.text = text;
+  Api.sendMessage(prms);
   return;
 }
 
 let task = new RunAllTask({ id: task_id });
-Api.sendMessage({
-  text: `<blockquote>Last Broadcast task status:</>\n\n<b>Task ID:</b> ${task.id}\n<b>Current Position:</b> ${task.cur_position}\n<b>Status Code:</b> ${task.status_code}\n<b>Progress:</b> ${task.progress}%\n<b>Created At:</b> ${task.created_at}\n<b>Total:</> ${task.total}\n<b>Speed:</> ${task.speed}\n\n<b>Status:</b> ${task.status}`,
-  parse_mode: "HTML",
-  reply_to_message_id: request.message_id,
-});
+prms.text = `<blockquote>Last Broadcast task status:</>\n\n<b>Task ID:</b> ${task.id}\n<b>Current Position:</b> ${task.cur_position}\n<b>Status Code:</b> ${task.status_code}\n<b>Progress:</b> ${task.progress}%\n<b>Created At:</b> ${task.created_at}\n<b>Total:</> ${task.total}\n<b>Speed:</> ${task.speed}\n\n<b>Status:</b> ${task.status}`
+
+Api.sendMessage(prms);
