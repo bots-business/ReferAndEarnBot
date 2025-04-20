6ce298555839b950bc935358dc8b62ef5528dbdd
Bots.Business <git.sync@bots.business>
2025-04-20 18:56:32 +0700

    refactorimg for refferal cmd

--- commands/referral /_referral.js
+++ commands/referral /_referral.js
@@ -17,28 +17,28 @@
 CMD*/
 
 // bonus and currency settings
-var referralBonus = SETTINGS.REFER_REWARD || 0.5;
-var currency = SETTINGS.CURRENCY || "TRX";
+const referralBonus = SETTINGS.REFER_REWARD || 0.5;
+const currency = SETTINGS.CURRENCY || "TRX";
 
 // Generate referral link
-var inviteLink = RefLib.getRefLink(
+const inviteLink = RefLib.getRefLink(
   bot.name,
   SETTINGS.REFER_LINK_PREFIX || "Bot"
 );
 
 // Image URL for link preview
-let imageUrl =
+const imageUrl =
   SETTINGS.REFER_IMAGE_URL ||
   "https://telegra.ph/file/48041b64392e58130f23a.jpg";
 
 // Prepare message content
-let refMessage = `<b>🎉 Total Referrals:</b> ${RefLib.getRefCount()} user(s)
+const refMessage = `<b>🎉 Total Referrals:</b> ${RefLib.getRefCount()} user(s)
 🔗 <b>Your Invite Link: </b><code>${inviteLink}</code>
 
 💰 <b>Earn ${referralBonus} ${currency}</b> for every successful referral!`;
 
 // Prepare inline buttons
-let buttons = {
+const buttons = {
   inline_keyboard: [
     [
       { text: "🔍 My Refers", callback_data: "/myreferrals" },
@@ -62,16 +62,18 @@ if (request.message?.message_id) {
     },
     reply_markup: buttons,
   });
-} else {
-  // Send new message if no message_id
-  Api.sendMessage({
-    text: refMessage,
-    parse_mode: "HTML",
-    link_preview_options: {
-      url: imageUrl,
-      prefer_large_media: true,
-      show_above_text: true,
-    },
-    reply_markup: buttons,
-  });
+  return
 }
+
+// Send new message if no message_id
+Api.sendMessage({
+  text: refMessage,
+  parse_mode: "HTML",
+  link_preview_options: {
+    url: imageUrl,
+    prefer_large_media: true,
+    show_above_text: true,
+  },
+  reply_markup: buttons,
+});
+
